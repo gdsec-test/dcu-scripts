386257b1fe313680d26cad008b52117b920d195f
Nick Wade <nwade@godaddy.com>
2023-02-15 16:08:38 -0600

    Use new on-call group

--- .github/snow_stats.sh
+++ .github/snow_stats.sh
@@ -5,17 +5,19 @@ SLACK_WEBHOOK_URL="$SLACK_WEBHOOK_URL"
 SLACK_CHANNEL="$SLACK_CHANNEL"
 SNOW_USER="$SNOW_USER"
 SNOW_PASS="$SNOW_PASS"
-ASSIGN_GROUP="4b80f9c10f1b8e009d232ca8b1050e20"
+ASSIGN_GROUP="95546b45db735194191d9e74f39619e9"
 
 send_slack_notification() {
     curl --silent -H 'Content-Type: application/json' --data "{\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"SNOW Stats\", \"as_user\": \"true\", \"link_names\": \"true\", \"icon_emoji\": \":snow:\", \"attachments\": [{\"pretext\":\"$1\", \"text\": \"$2\"}] }" ${SLACK_WEBHOOK_URL}
 }
 
 get_current_oncall() {
-    CURRENT_USER_ID=$(curl -u "$SNOW_USER:$SNOW_PASS" --silent --location --request GET "https://godaddy.service-now.com/api/now/on_call_rota/whoisoncall?group_ids=$ASSIGN_GROUP" --header 'Accept: application/json' --header 'Content-Type: application/json' | jq -r '.result[0].userId')
-    CURRENT_USER_NAME=$(curl -u "$SNOW_USER:$SNOW_PASS" --silent --location --request GET https://godaddy.service-now.com/api/now/table/sys_user/$CURRENT_USER_ID --header 'Accept: application/json' --header 'Content-Type: application/json' | jq -r '.result.user_name')
+    PRIMARY_USER_ID=$(curl -u "$SNOW_USER:$SNOW_PASS" --silent --location --request GET "https://godaddy.service-now.com/api/now/on_call_rota/whoisoncall?group_ids=$ASSIGN_GROUP" --header 'Accept: application/json' --header 'Content-Type: application/json' | jq -r '.result[0].userId')
+    PRIMARY_USER_NAME=$(curl -u "$SNOW_USER:$SNOW_PASS" --silent --location --request GET https://godaddy.service-now.com/api/now/table/sys_user/$PRIMARY_USER_ID --header 'Accept: application/json' --header 'Content-Type: application/json' | jq -r '.result.user_name')
+    SECONDARY_USER_ID=$(curl -u "$SNOW_USER:$SNOW_PASS" --silent --location --request GET "https://godaddy.service-now.com/api/now/on_call_rota/whoisoncall?group_ids=$ASSIGN_GROUP" --header 'Accept: application/json' --header 'Content-Type: application/json' | jq -r '.result[1].userId')
+    SECONDARY_USER_NAME=$(curl -u "$SNOW_USER:$SNOW_PASS" --silent --location --request GET https://godaddy.service-now.com/api/now/table/sys_user/$SECONDARY_USER_ID --header 'Accept: application/json' --header 'Content-Type: application/json' | jq -r '.result.user_name')
 
-    send_slack_notification "On-call Today" "@$CURRENT_USER_NAME"
+    send_slack_notification "On-call Today(Primary/Secondary)" "@$PRIMARY_USER_NAME/@$SECONDARY_USER_NAME"
 }
 
 get_current_incidents() {
