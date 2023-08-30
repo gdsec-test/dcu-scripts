6a2bbd5dbe305eac896933d87f421058e2010917
lmcphie-godaddy <105673626+lmcphie-godaddy@users.noreply.github.com>
2023-08-30 12:13:26 -0400

    added CI script (#110)
    * added CI script

--- .github/github_settings.sh
+++ .github/github_settings.sh
@@ -2,76 +2,109 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+check_repo_list() {
+
+    repos="$2"
+    if [ $# -ne 2 ]; then
+        echo "Error: Exactly 2 argument required!"
+        break
+    fi
+    while read -r line; do
+        if [[ $repos == "repos_unique.txt" && "$1" == "$line" ]]; then
+            echo "No ci script will be added!"
+            break
+        fi
+        if [ "$1" == "$line" ]; then 
+            echo 0 
+        fi
+    done <<<"$(cat $SCRIPT_DIR/"$2")"
+}
+
 set_github_stats() {
     while read repo; do
-        gh repo clone $repo
-        pushd $(basename $repo) || exit 1
+       gh repo clone $repo
+       pushd $(basename $repo) || exit 1
 
-        owner="$(dirname $repo)"
-        name="$(basename $repo)"
-        repositoryId="$(gh api graphql -f query='{repository(owner:"'$owner'",name:"'$name'"){id}}' -q .data.repository.id)"
-        gh api graphql -f query="
-        query {
-            repository(owner: \"$owner\", name: \"$name\") {
-                branchProtectionRules(first:100) {
-                    nodes {
-                        pattern
-                        id
-                    }
-                    pageInfo {
-                        endCursor
-                        hasNextPage
-                    }
-                }
-            }
-        }" | jq -r '.data.repository.branchProtectionRules.nodes[].id' | while read line ; do gh api graphql -f query="mutation {deleteBranchProtectionRule(input:{branchProtectionRuleId: \"$line\"}) {clientMutationId}}" ; done
-        gh api graphql -f query='
-        mutation($repositoryId:ID!,$branch:String!) {
-        createBranchProtectionRule(input: {
-            repositoryId: $repositoryId
-            pattern: $branch
-            requiredApprovingReviewCount: 2
-            requiresConversationResolution: true
-            requiresCodeOwnerReviews: true
-            requiresApprovingReviews: true
-            dismissesStaleReviews: true
-            restrictsPushes: true
-            requiredStatusCheckContexts: []
-            requiresStatusChecks:true
-        }) { clientMutationId }
-        }' -f repositoryId="$repositoryId" -f branch="main"
-        gh api graphql -f query='
-        mutation($repositoryId:ID!,$branch:String!) {
-        createBranchProtectionRule(input: {
-            repositoryId: $repositoryId
-            pattern: $branch
-            requiredApprovingReviewCount: 2
-            requiresConversationResolution: true
-            requiresCodeOwnerReviews: true
-            requiresApprovingReviews: true
-            dismissesStaleReviews: true
-            restrictsPushes: true
-            requiredStatusCheckContexts: []
-            requiresStatusChecks:true
-        }) { clientMutationId }
-        }' -f repositoryId="$repositoryId" -f branch="master"
+       owner="$(dirname $repo)"
+       name="$(basename $repo)"
+       repositoryId="$(gh api graphql -f query='{repository(owner:"'$owner'",name:"'$name'"){id}}' -q .data.repository.id)"
+       gh api graphql -f query="
+       query {
+           repository(owner: \"$owner\", name: \"$name\") {
+               branchProtectionRules(first:100) {
+                   nodes {
+                       pattern
+                       id
+                   }
+                   pageInfo {
+                       endCursor
+                       hasNextPage
+                   }
+               }
+           }
+       }" | jq -r '.data.repository.branchProtectionRules.nodes[].id' | while read line ; do gh api graphql -f query="mutation {deleteBranchProtectionRule(input:{branchProtectionRuleId: \"$line\"}) {clientMutationId}}" ; done
+       gh api graphql -f query='
+       mutation($repositoryId:ID!,$branch:String!) {
+       createBranchProtectionRule(input: {
+           repositoryId: $repositoryId
+           pattern: $branch
+           requiredApprovingReviewCount: 2
+           requiresConversationResolution: true
+           requiresCodeOwnerReviews: true
+           requiresApprovingReviews: true
+           dismissesStaleReviews: true
+           restrictsPushes: true
+           requiredStatusCheckContexts: []
+           requiresStatusChecks:true
+       }) { clientMutationId }
+       }' -f repositoryId="$repositoryId" -f branch="main"
+       gh api graphql -f query='
+       mutation($repositoryId:ID!,$branch:String!) {
+       createBranchProtectionRule(input: {
+           repositoryId: $repositoryId
+           pattern: $branch
+           requiredApprovingReviewCount: 2
+           requiresConversationResolution: true
+           requiresCodeOwnerReviews: true
+           requiresApprovingReviews: true
+           dismissesStaleReviews: true
+           restrictsPushes: true
+           requiredStatusCheckContexts: []
+           requiresStatusChecks:true
+       }) { clientMutationId }
+       }' -f repositoryId="$repositoryId" -f branch="master"
 
-        mkdir -p .github/
-        cp $SCRIPT_DIR/pull_request_template.md .github/pull_request_template.md
-        cp $SCRIPT_DIR/../CODEOWNERS CODEOWNERS
-        # If there are changes in the repo create a PR.
-        if [[ `git status --porcelain | wc -l` -gt 0 ]]; 
-        then
-            bname=github-settings-sync-$(date +"%Y-%M-%d_%H%M%S")
-            git checkout -b $bname
-            git add .
-            git commit -m "Bringing inline with dcu-scripts"
-            git push --set-upstream origin $bname
-            gh pr create --title "Sync the repo settings" --body "Bringing inline with dcu-scripts"
-        fi
-        popd
-        rm -rf $(basename $repo)
-    done <<<"$(cat $SCRIPT_DIR/repos.txt)"
+
+       mkdir -p .github/
+       cp $SCRIPT_DIR/pull_request_template.md .github/pull_request_template.md
+       cp $SCRIPT_DIR/../CODEOWNERS CODEOWNERS
+
+       check_repo_golang=$(check_repo_list $repo "repos_golang.txt")
+       check_repo_empty=$(check_repo_list $repo "repos_empty.txt")
+       check_repo_unique=$(check_repo_list $repo "repos_unique.txt")
+       if [[ $check_repo_empty == 0 ]]; then
+            cp $SCRIPT_DIR/workflows/ci.yaml .github/workflows/ci.yaml
+       elif [[ $check_repo_golang == 0 ]]; then
+            cp $SCRIPT_DIR/go-ci.yaml .github/workflows/ci.yaml
+       elif [[ $check_repo_unique == "No ci script will be added!" ]]; then
+            echo "No ci script copied for:" $repo
+       else
+            cp $SCRIPT_DIR/py-ci.yaml .github/workflows/ci.yaml
+       fi
+
+       # If there are changes in the repo create a PR.
+       if [[ `git status --porcelain | wc -l` -gt 0 ]];
+       then
+           bname=github-settings-sync-$(date +"%Y-%M-%d_%H%M%S")
+           git checkout -b $bname
+           git add .
+           git commit -m "Bringing inline with dcu-scripts"
+           git push --set-upstream origin $bname
+           gh pr create --title "Sync the repo settings" --body "Bringing inline with dcu-scripts"
+       fi
+       popd
+       rm -rf $(basename $repo)
+   done <<<"$(cat $SCRIPT_DIR/repos.txt)"
 }
 
 set_github_stats
--- README.md
+++ README.md
@@ -8,3 +8,15 @@ If you need to lock or change a shopper account's password or send a templated e
 
 #### Close Tickets
 If you need to close a number of tickets given parameters that can be formulated into a db query, [this is the script](close_tickets) you want.
+
+# Run the Github Settings Bash Script
+#### ADDING A NEW REPO
+Repos_golang is a list of golang repos with unit-test & test coverage. \
+Repos_empty is a list of python repos with empty unit-test & test coverage. \
+Repos_unique is a list of repos that need a unique ci script or none at all.
+
+Set the codeQL analysis to 'advanced' in the settings of the repo. \
+Add the repo name to the regular repos list and to the specific repo list if it fits said description. \
+Then, run github_settings.sh. 
+
+If the repo is unique and its ci script needs updating, it has to be manually updated.
